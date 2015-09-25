b90095ac2a4c8b32aafb6bb7c5b4a4408e602989
duergner <michael@pocketsunited.com>
2015-09-25 10:48:19 +0200

    Initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -5,19 +5,113 @@
 
     var app = angular.module('io.duergner.angular-dummy');
 
-    var authenticationService = app.factory('AuthenticationService',[
-        '$q',
+    /**
+     * This is a simple CredentialStorage; it's needed as AngularJS cannot resolve circular dependencies and used as a
+     * man in the middle for AuthenticationInterceptor and AuthenticationService
+     *
+     * @type {*|Object}
+     */
+    var credentialStorage = app.factory('CredentialStorage',[
         '$log',
-        function($q,$log) {
+        function($log) {
             return {
                 _token: null,
 
+                token: function(token) {
+                    if (undefined !== token && null !== token) {
+                        this._token = token;
+                    }
+                    return this._token;
+                },
+
+                hasToken: function() {
+                    return undefined !== this._token && null !== this._token;
+                }
+            }
+        }
+    ]);
+
+    /**
+     * This is our basic service that can handle login / logout and alike
+     *
+     * @type {*|Object}
+     */
+    var authenticationService = app.factory('AuthenticationService',[
+        '$q',
+        '$http',
+        'CredentialStorage',
+        '$log',
+        function($q,$http,credentialStorage,$log) {
+            return {
+                _credentialStorage: credentialStorage,
+
+                /**
+                 * This method can be called by the LoginController upon form submit
+                 *
+                 * @param email
+                 * @param password
+                 * @returns {*}
+                 */
+                loginWithEmail: function(email,password) {
+                    var defer = $q.defer(),
+                        self = this;
+
+                    $http.post(API_BASE + '/login',{email: email, password: password}).then(function(response) {
+                        if (200 === response.status) {
+                            if (response.status.success) {
+                                if (-1 < response.data.content.roles.indexOf('admin')) {
+                                    self._credentialStorage.token(response.data.content.sessionId);
+                                    defer.resolve();
+                                }
+                                else {
+                                    defer.reject('You\'re not an admin!');
+                                }
+                            }
+                            else {
+                                defer.reject(response.data.errorMessage);
+                            }
+                        }
+                        else {
+                            defer.reject(response.statusText);
+                        }
+                    },function(response) {
+                        defer.reject(response.statusText);
+                    });
+
+                    return defer.promise
+                },
+                logout: function() {
+                    var defer = $q.defer();
+
+                    this._credentialStorage.token(null);
+                    defer.resolve();
+
+                    return defer.promise;
+                }
+            }
+        }
+    ]);
+
+    /**
+     * This class will intercept all HTTP calls and add Authentication header on outgoing requests and will redirect to
+     * login page upon 401 HTTP status codes
+     *
+     * @type {*|Object}
+     */
+    var httpAuthenticationInterceptor = app.factory('AuthenticationInterceptor',[
+        '$q',
+        'CredentialStorage',
+        '$log',
+        function($q,credentialStorage,$log) {
+            return {
+                _credentialStorage: credentialStorage,
+
                 request: function(config) {
-                    if (0 === config.url.indexOf(API_BASE) && null !== token) {
+                    if (0 === config.url.indexOf(API_BASE) && this._credentialStorage.hasToken()) {
                         if (undefined === config.headers || null == config.headers) {
                             config.headers = {}
                         }
-                        config.headers.Authorization = 'Bearer ' + this._token;
+                        config.headers.Authorization = 'Bearer ' + this._credentialStorage.token();
                     }
                     return config;
                 },
@@ -32,6 +126,11 @@
         }
     ]);
 
+    /**
+     * This is a simple showcase business layer service which is responsible for handling inviations
+     *
+     * @type {*|Object}
+     */
     var invitationService = app.factory('InvitationService',[
         '$http',
         '$q',
@@ -103,6 +202,10 @@
         }
     ]);
 
+    /**
+     * This is our very basic controller to handle the invitation list page
+     *
+     */
     var invitationController = app.controller('InvitationController',[
         '$scope',
         'InvitationService',
@@ -120,10 +223,13 @@
         }
     ]);
 
+    /**
+     * Add our custom HTTP interceptor
+     */
     app.configure([
         '$httpProvider',
         function($httpProvider) {
-            $httpProvider.interceptors.push('AuthenticationService');
+            $httpProvider.interceptors.push('AuthenticationInterceptor');
         }
     ]);
 
